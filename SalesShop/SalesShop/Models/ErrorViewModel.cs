330964f1c7112e40e5e484effa9dbbfb740484f1
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-12-24 09:20:18 +0700

    initial project

---
+++ SalesShop/SalesShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
